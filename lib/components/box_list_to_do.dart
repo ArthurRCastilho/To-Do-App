133a2453ba2b7765ad5704dc7432c6f6c884d54c
ArthurLVidal <arthurodri2017@gmail.com>
2024-08-01 11:29:10 -0300

    Criando botão de checkList

--- lib/components/box_list_to_do.dart
+++ lib/components/box_list_to_do.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:to_do_app/models/to_do.dart';
 
-class BoxListToDo extends StatelessWidget {
+class BoxListToDo extends StatefulWidget {
   final ToDo toDo;
 
   const BoxListToDo({
     super.key,
     required this.toDo,
   });
+
+  @override
+  State<BoxListToDo> createState() => _BoxListToDoState();
+}
+
+class _BoxListToDoState extends State<BoxListToDo> {
+  bool isCheck = false;
 
   @override
   Widget build(BuildContext context) {
@@ -29,7 +36,7 @@ class BoxListToDo extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              toDo.title,
+              widget.toDo.title,
               style: const TextStyle(
                 color: Colors.black,
                 fontSize: 20,
@@ -39,19 +46,33 @@ class BoxListToDo extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Text(toDo.description),
-                const CircleAvatar(
-                  backgroundColor: Color(0xff7F7F7F),
+                Text(widget.toDo.description),
+                Container(
+                  height: 40,
+                  width: 40,
+                  child: FloatingActionButton(
+                    backgroundColor:
+                        isCheck ? Colors.green : const Color(0xff7F7F7F),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        isCheck = !isCheck;
+                      });
+                    },
+                    child: Icon(Icons.check),
+                  ),
                 ),
               ],
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.end,
               children: [
-                toDo.date == null
-                    ? SizedBox()
+                widget.toDo.date == null
+                    ? const SizedBox()
                     : Text(
-                        '${toDo.date!.day}/${toDo.date!.month}/${toDo.date!.year}',
+                        '${widget.toDo.date!.day}/${widget.toDo.date!.month}/${widget.toDo.date!.year}',
                       ),
                 const SizedBox(width: 40),
               ],
