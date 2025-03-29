37e74cb22117fefa5f940fd3e42903d2bd232f91
ArthurLVidal <arthurodri2017@gmail.com>
2025-03-29 00:48:42 -0300

    Add Splash Screen e IconApp

--- lib/core/models/to_do.dart
+++ lib/core/models/to_do.dart
@@ -7,16 +7,37 @@ class ToDo with ChangeNotifier {
   String title;
   String description;
   DateTime? date;
-  bool isConclued = false;
+  bool isConclued;
 
   ToDo({
     String? id,
     required this.title,
     required this.description,
     this.date,
+    this.isConclued = false,
   }) : id = id ?? Random().nextDouble().toString();
 
   bool toggleIsConclued() {
     return isConclued = !isConclued;
+  }
+
+  // Converte o objeto para JSON
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'description': description,
+      'date': date?.toIso8601String(),
+      'isConclued': isConclued,
+    };
+  }
+
+  // Construtor que cria um objeto a partir de um JSON
+  factory ToDo.fromJson(Map<String, dynamic> json) {
+    return ToDo(
+      title: json['title'],
+      description: json['description'],
+      date: json['date'] != null ? DateTime.parse(json['date']) : null,
+      isConclued: json['isConclued'] ?? false,
+    );
   }
 }
--- lib/core/services/to_do_mock_service.dart
+++ lib/core/services/to_do_mock_service.dart
@@ -1,16 +1,40 @@
 import 'dart:async';
+import 'dart:convert';
 
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:to_do_app/core/services/to_do_service.dart';
 import 'package:to_do_app/core/models/to_do.dart';
 
 class ToDoMockService implements ToDoService {
   static final List<ToDo> _toDos = [];
   static MultiStreamController<List<ToDo>>? _controller;
+  static bool _initialized = false; // Flag to check if the data is loaded
 
   static final _toDoStream = Stream<List<ToDo>>.multi((controller) {
     _controller = controller;
-    controller.add(_toDos);
+    if (!_initialized) {
+      _loadToDos().then((_) {
+        controller.add(_toDos);
+        _initialized = true; // Mark as initialized after loading
+      });
+    }
   });
+
+  static Future<void> _loadToDos() async {
+    final prefs = await SharedPreferences.getInstance();
+    final toDosJson = prefs.getString('toDos');
+    if (toDosJson != null) {
+      final List<dynamic> decoded = jsonDecode(toDosJson);
+      _toDos.clear();
+      _toDos.addAll(decoded.map((e) => ToDo.fromJson(e)));
+    }
+  }
+
+  static Future<void> _saveToDos() async {
+    final prefs = await SharedPreferences.getInstance();
+    final toDosJson = jsonEncode(_toDos.map((e) => e.toJson()).toList());
+    await prefs.setString('toDos', toDosJson);
+  }
 
   Stream<List<ToDo>> toDoStream() {
     return _toDoStream;
@@ -18,11 +42,13 @@ class ToDoMockService implements ToDoService {
 
   Future<void> remove(ToDo toDo) async {
     _toDos.remove(toDo);
+    await _saveToDos();
     _controller?.add(_toDos);
   }
 
   Future<void> conclued(ToDo toDo) async {
     toDo.toggleIsConclued();
+    await _saveToDos();
     _controller?.add(_toDos);
   }
 
@@ -38,7 +64,7 @@ class ToDoMockService implements ToDoService {
     );
 
     _toDos.add(newToDo);
-
+    await _saveToDos();
     _controller?.add(_toDos);
     return newToDo;
   }
